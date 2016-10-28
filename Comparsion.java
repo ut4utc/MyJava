ca18b7dd2a7717c412ef723c5f212afad5951a4e
ut4utc <ut4utc@gmail.com>
2016-10-28 21:54:27 +0300

    Comparsion
    javac Comparsion.java
    java Comparsion

---
+++ Comparsion.java
@@ -0,0 +1,26 @@
+class Comparsion {
+
+	public static void main ( String[] args ) 
+	{
+		String txt = "Fantastic ";
+		String lang = "Java";
+		
+		boolean state = (txt == lang);
+		System.out.println("Проверка строк на равенство: " + state );
+		
+		state = (txt != lang);
+		System.out.println("Проверка строк на неравенство: " + state );
+		
+		int dozen = 12;
+		int score = 20;
+		
+		state = (dozen > score );
+		System.out.println("Проверка на больше: " + state );
+		
+		state = (dozen < score );
+		System.out.println("Проверка на меньше: " + state );
+		
+	}
+
+
+}
