02718f2eb6cde2b575e8ada3fb62714cd1af22b0
ut4utc <ut4utc@gmail.com>
2016-10-31 22:46:58 +0200

    Escape
    javac Escape.java
    java Escape

---
+++ Escape.java
@@ -0,0 +1,13 @@
+class Escape {
+  public static void main(String[] args) {
+    String header = "\n\tНью-Йорк Прогноз на 3 дня:\n";
+    header += "\n\tДень\t\tМакс\tМин\tОсадки\n";
+    header += "\t---\t\t----\t--------------\n";
+
+    String forecast = "\tВоскресенье\t68F\t48F\tЯсно\n";
+    forecast += "\tВторник\t\t71F\t50F\tОблачность\n";
+
+    System.out.println( header + forecast );
+  }
+
+}
