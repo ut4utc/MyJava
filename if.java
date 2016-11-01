8dac7b2216e963d0f90c239e47fb0b8b3cdeff78
ut4utc <ut4utc@gmail.com>
2016-11-01 22:25:14 +0200

    If
    javac If.java
    java If

---
+++ if.java
@@ -0,0 +1,10 @@
+class If {
+  public static void main(String[] args) {
+    if (5>1) System.out.println("5 more than 1");
+    if (2<4) {
+      System.out.println("Два меньше четырех");
+      System.out.println("Проверка выполнена успешно");
+    }
+  }
+
+}
