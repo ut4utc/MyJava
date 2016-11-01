4d74f4864b322da727ecc6856283e63e6aef052e
ut4utc <ut4utc@gmail.com>
2016-11-01 22:42:48 +0200

    fix

--- Else.java
+++ Else.java
@@ -1,9 +1,13 @@
 class Else {
   public static void main(String[] args) {
-    int hrs = 11;
+    int hrs = 15;
     if ( hrs < 13 )  {
       System.out.println("Доброе утро " + hrs );
     }
+    else if (hrs < 18) {
+      System.out.println("Добрый день " + hrs );
+    }
+
   }
 
 }
