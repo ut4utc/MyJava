b593da0e93c346734555d0dffca845e399171b6c
ut4utc <ut4utc@gmail.com>
2016-11-03 23:34:06 +0200

    While
    javac While.java
    java While

---
+++ While.java
@@ -0,0 +1,10 @@
+class While {
+  public static void main(String[] args) {
+    int num = 100;
+    while (num > 0) {
+      System.out.println("Обратный отсчет с while: " + num );
+      num -= 10;
+    }
+    
+  }
+}
