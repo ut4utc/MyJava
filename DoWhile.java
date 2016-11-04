4f4ce99645a0816dbfb2e6dc487d962570171bc2
ut4utc <ut4utc@gmail.com>
2016-11-04 06:52:46 +0200

    DoWhile
    javac DoWhile.java
    java DoWhile

---
+++ DoWhile.java
@@ -0,0 +1,11 @@
+class DoWhile {
+  public static void main (String[] args){
+    int num = 100;
+    do {
+      System.out.print("Используем DoWhile: " + num + "\n");
+      num += 10;
+    } while (num<0);
+
+  }
+
+}
