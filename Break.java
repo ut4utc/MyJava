80c875f7c7583db43d7227630512e78f2d87caa9
ut4utc <ut4utc@gmail.com>
2016-11-04 07:31:32 +0200

    add continuos

--- Break.java
+++ Break.java
@@ -2,12 +2,18 @@ class Break {
   public static void main(String[] args) {
     for (int i=1; i<=4; i++) {
       System.out.println("\n");
+
       for (int j=1; j<=4; j++) {
+        if (i == 3 && j == 3) {
+        //  System.out.println("-- Пропуск итерации i=" +i+" j="+j);
+          continue;
+        }
         System.out.println("Итерация i=" +i+" j="+j);
         if (i==2 && j==1) {
           System.out.println("Выход из внутреннего цикла i=" +i+" j="+j);
           break;
         }
+
       }
     }
 
