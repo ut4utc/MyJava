72ce89ed618a65824ad6f6815a74361311255973
ut4utc <ut4utc@gmail.com>
2016-11-04 07:22:01 +0200

    fix break

--- Break.java
+++ Break.java
@@ -4,6 +4,10 @@ class Break {
       System.out.println("\n");
       for (int j=1; j<=4; j++) {
         System.out.println("Итерация i=" +i+" j="+j);
+        if (i==2 && j==1) {
+          System.out.println("Выход из внутреннего цикла i=" +i+" j="+j);
+          break;
+        }
       }
     }
 
