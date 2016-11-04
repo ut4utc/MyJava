2ec294828a0c28910d1d0d438c47906a78f5af30
ut4utc <ut4utc@gmail.com>
2016-11-04 07:18:42 +0200

    Break
    javac Break.java
    java Break

---
+++ Break.java
@@ -0,0 +1,12 @@
+class Break {
+  public static void main(String[] args) {
+    for (int i=1; i<=4; i++) {
+      System.out.println("\n");
+      for (int j=1; j<=4; j++) {
+        System.out.println("Итерация i=" +i+" j="+j);
+      }
+    }
+
+
+  }
+}
