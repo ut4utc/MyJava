9c5fb5c9b556193f180375fc35c60736bc12957d
ut4utc <ut4utc@gmail.com>
2016-11-08 22:54:40 +0200

    Loops
    javac Loops.java
    java Loops This is my Life
    args[0] - | This
    args[1] - | is
    args[2] - | my
    args[3] - | Life

---
+++ Loops.java
@@ -0,0 +1,12 @@
+class Loops {
+  public static void main(String args[]){
+    if (args.length > 0){
+      for (int i = 0; i < args.length; i++ ) {
+        System.out.println("args[" +i+"] - | "+args[i] );
+      } // end for
+
+    }// end if
+
+  }
+
+}
